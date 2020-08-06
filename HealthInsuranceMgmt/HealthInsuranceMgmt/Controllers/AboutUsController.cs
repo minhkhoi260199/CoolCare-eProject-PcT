b9a76b56a543b744713515e710bb14b2f2d946b1
Kayz <thevinhlam1996@gmail.com>
2020-08-07 02:53:35 +0700

    no message

---
+++ HealthInsuranceMgmt/HealthInsuranceMgmt/Controllers/AboutUsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthInsuranceMgmt.Controllers
+{
+    [Route("aboutUs")]
+    public class AboutUsController : Controller
+    {
+        [Route("")]
+        [Route("/index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HealthInsuranceMgmt/HealthInsuranceMgmt/Controllers/ContactUsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthInsuranceMgmt.Controllers
+{   [Route("contactUs")]
+    public class ContactUsController : Controller
+    {
+        [Route("")]
+        [Route("/index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
