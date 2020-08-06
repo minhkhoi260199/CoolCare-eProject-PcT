b14f01d8fe23478e3eca6bca611f2ff7e1a810e9
KhoiNg <minhkhoi260199@gmail.com>
2020-08-07 00:06:44 +0700

    emp profile

--- HealthInsuranceMgmt/HealthInsuranceMgmt/Controllers/EmployeeController.cs
+++ HealthInsuranceMgmt/HealthInsuranceMgmt/Controllers/EmployeeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using HealthInsuranceMgmt.Models.Respositories;
+using HealthInsuranceMgmt.Models;
 using Microsoft.AspNetCore.Http;
 
 namespace HealthInsuranceMgmt.Controllers
@@ -12,11 +13,12 @@ namespace HealthInsuranceMgmt.Controllers
     public class EmployeeController : Controller
     {
         private IPoliciesOnEmployeesResponsitory ipoliciesOnEmployeesResponsitory;
-        private IPoliciesResponsitory ipoliciesResponsitory;
+        private IEmployeesResponsitory iemployeesResponsitory;
         
-        public EmployeeController(IPoliciesOnEmployeesResponsitory _ipoliciesOnEmployeesResponsitory)
+        public EmployeeController(IPoliciesOnEmployeesResponsitory _ipoliciesOnEmployeesResponsitory,IEmployeesResponsitory _iemployeesResponsitory)
         {
             ipoliciesOnEmployeesResponsitory = _ipoliciesOnEmployeesResponsitory;
+            iemployeesResponsitory = _iemployeesResponsitory;
         }
 
         [Route("")]
@@ -30,29 +32,51 @@ namespace HealthInsuranceMgmt.Controllers
         }
 
         [Route("profile")]
-        public IActionResult Profile()
+        public async Task<IActionResult> Profile()
         {
             ViewBag.pageTitle = HttpContext.Session.GetString("userName") + " Profile";
-
-            return View("profile");
+            var id = int.Parse(HttpContext.Session.GetString("userId"));
+            var employee = await iemployeesResponsitory.GetById(id);
+            return View("profile", employee);
         }
 
+        [HttpPost]
+        [Route("submitedit")]
+        public async Task<IActionResult> SubmitEditProfile(Employees employee)
+        {
+            var oldEmployee = await iemployeesResponsitory.GetById(employee.Id);
+            employee.Status = oldEmployee.Status;
+            if (oldEmployee.JoinDate != null && oldEmployee.JoinDate.ToString() != "")
+            {
+                employee.JoinDate = oldEmployee.JoinDate;
+            }
+            else
+            {
+                employee.JoinDate = DateTime.Now;
+            }
+            if(employee.Password != "" && employee.Status != 1 && employee.Password != null)
+            {
+                employee.Status = 2;
+            }
+            else
+            {
+                if(oldEmployee.Password != "")
+                {
+                    employee.Password = oldEmployee.Password;
+                }
+            }
+            try {
+                var checkSal = decimal.Parse(employee.Salary.ToString());
+            } catch(Exception e) {
+                ModelState.AddModelError("salary", "Please enter a number.");
+            }
+            if (ModelState.IsValid)
+            {
+                await iemployeesResponsitory.Update(employee.Id, employee);
+                return View("success");
+            }
+            return RedirectToAction("profile","employee");
+        }
         
-        [Route("editprofile")]
-        public IActionResult EditProfile()
-        {
-            ViewBag.pageTitle = HttpContext.Session.GetString("userName") + " Profile";
-
-            return View("profile");
-        }
-        
-        [Route("password")]
-        public IActionResult ChangePass()
-        {
-            ViewBag.pageTitle = HttpContext.Session.GetString("userName") + " Profile";
-
-            return View("profile");
-        }
-
     }
 }
